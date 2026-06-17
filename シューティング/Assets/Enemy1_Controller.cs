dded69a5cf430b10787f3515e02f6efd1dc59003
ouga7002 <01aouga7002@gmail.com>
2026-06-17 09:47:16 +0900

    戻したい

--- シューティング/Assets/Enemy1_Controller.cs
+++ シューティング/Assets/Enemy1_Controller.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class Enemy1_Controller : MonoBehaviour
 {
+    [SerializeField] float speed = 5.0f;
     private void OnCollisionEnter2D(Collision2D collision) //ぶつかったら消える命令文開始
     {
         Destroy(gameObject);//消滅
