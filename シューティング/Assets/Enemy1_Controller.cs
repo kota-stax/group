2fcea5c797a67d609f133c01d836065447fe077a
ouga7002 <01aouga7002@gmail.com>
2026-06-22 16:45:45 +0900

    敵　弾　出したかった

--- シューティング/Assets/Enemy1_Controller.cs
+++ シューティング/Assets/Enemy1_Controller.cs
@@ -1,4 +1,3 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Enemy1_Controller : MonoBehaviour
@@ -6,12 +5,41 @@ public class Enemy1_Controller : MonoBehaviour
     [SerializeField] float speed = 5.0f;
     [SerializeField] int hp = 1;
 
-    private void OnCollisionEnter2D(Collision2D collision) //ぶつかったら消える命令文開始
+    [Header("射撃設定")]
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] Transform firePoint;
+    [SerializeField] float shotInterval = 2f;
+
+    private float shotTimer = 0f;
+
+    void Update()
+    {
+        // 射撃タイマー
+        shotTimer += Time.deltaTime;
+
+        if (shotTimer >= shotInterval)
+        {
+            Shoot();
+            shotTimer = 0f;
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(
+            bulletPrefab,
+            firePoint.position,
+            Quaternion.identity
+        );
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         hp--;
+
         if (hp <= 0)
         {
-            Destroy(gameObject);//消滅
-        }       
+            Destroy(gameObject);
+        }
     }
 }
